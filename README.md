84c3df0163296df385dad2c57314d0c2e0a5a3fd
Explosion-Scratch <explosion.scratch@gmail.com>
2022-04-16 20:51:35 +0000

    v1.0.3

---
+++ README.md
@@ -0,0 +1,13 @@
+# HTMLify CLI
+
+A beautiful full featured command line interface to download Scratch projects locally as HTML files.
+
+## Usage:
+
+```bash
+# PNPM is better, but you do you
+npm i -g htmlifycli
+
+# It's interactive, just follow the prompts
+htmlifycli
+```
--- index.js
+++ index.js
@@ -1,11 +1,226 @@
 import fs from "fs";
-import Htmlifier from "@sheeptester/htmlifier";
 import fetch from "node-fetch";
 import colors from "colors";
 import inquirer from "inquirer";
-import logUpdate from 'log-update';
+import logUpdate from "log-update";
+import { resolve } from "path";
+import Packager from "@turbowarp/packager";
 
 (async () => {
+  const CURRENT = {
+    logInterval: null,
+    status: "Waiting for user input",
+    projects: {},
+  };
+
+  const _slist = {
+    id: "A single project from an ID",
+    studio: "From a studio",
+    user: "From a user's projects",
+  };
+  let source = await inquirer.prompt([
+    {
+      name: "source",
+      type: "list",
+      choices: Object.values(_slist),
+      default: _slist.user,
+    },
+  ]);
+  source = Object.entries(_slist).find(([key, val]) => val == source.source)[0];
+
+  const sources = {
+    user: [
+      {
+        type: "input",
+        name: "user",
+        message: "What user's projects would you like to download?",
+        default: "griffpatch",
+      },
+      {
+        type: "number",
+        name: "limit",
+        message: "What number of projects should be maximum?",
+        default: 100,
+      },
+    ],
+    studio: [
+      {
+        type: "number",
+        name: "id",
+        message:
+          "What is the studio ID of the studio you want to download projects from?",
+        default: 28381459,
+      },
+      {
+        type: "number",
+        name: "limit",
+        message: "What number of projects should be maximum?",
+        default: 100,
+      },
+    ],
+    id: [
+      {
+        type: "number",
+        name: "id",
+        default: 60917032,
+        message: "What's the project ID of the project?",
+      },
+    ],
+  };
+
+  const sourceOptions = await inquirer.prompt([
+    ...sources[source],
+    { type: "number", name: "limit", default: 100, message: "Project limit" },
+    // { type: "list", choices: ["zip", "html"], default: "html" },
+  ]);
+
+  const features = {
+    turbo: "Turbo mode",
+    highQualityPen: "High quality pen?",
+    fencing: "Fence sprites?",
+    miscLimits: "Miscellaneous limits",
+    autoplay: "Autoplay",
+    loadingBar: "Show loading progress",
+    compilerEnable: "Enable compiler",
+    interpolation: "Interpolation",
+  };
+
+  const projectOptions = await inquirer.prompt([
+    {
+      type: "input",
+      name: "folder",
+      message: "Folder to save projects in (will be created if non-existant)",
+      default: sourceOptions.user || "projects",
+    },
+    {
+      type: "number",
+      name: "framerate",
+      message: "What frame rate should the project(s) run at? ",
+      default: 30,
+    },
+    {
+      type: "checkbox",
+      name: "features",
+      message: "What do you want enabled?",
+      choices: Object.values(features),
+      default: [
+        features.compilerEnable,
+        features.loadingBar,
+        features.fencing,
+        features.interpolation,
+        features.miscLimits,
+        features.autoplay,
+      ],
+    },
+  ]);
+
+  const opts = { source, sourceOptions, projectOptions };
+  CURRENT.logInterval = setInterval(log, 50);
+  CURRENT.status = "Fetching projects";
+
+  let projects = await (async () => {
+    if (opts.source === "id") {
+      return [
+        await fetch(
+          `https://api.scratch.mit.edu/projects/${opts.sourceOptions.id}`
+        ).then((res) => res.json()),
+      ];
+    } else {
+      let out = [];
+      let current = { length: 20 };
+      let i = 0;
+      while (current.length >= 20 && out.length < opts.sourceOptions.limit) {
+        CURRENT.status = `Getting page ${i}`.brightBlue;
+        let url = {
+          user: () => `users/${opts.sourceOptions.user}/projects?`,
+          studio: () => `studios/${opts.sourceOptions.id}/projects/?limit=20&`,
+          id: () => `projects/${opts.sourceOptions.id}`,
+        };
+        current = await fetch(
+          `https://api.scratch.mit.edu/${url[opts.source]()}offset=${20 * i++}`
+        ).then((res) => res.json());
+        out.push(...current);
+      }
+      return out.slice(0, opts.sourceOptions.limit);
+    }
+  })();
+  CURRENT.projects = Object.fromEntries(
+    projects.map((i) => [i.id, "Fetching".brightBlue])
+  );
+  let folder = opts.projectOptions.folder.replace(/\/$/, "");
+  CURRENT.status = "Creating folder";
+  fs.mkdirSync(folder, { recursive: true });
+  CURRENT.status = "Getting projects";
+  let projectPromises = [];
+  for (let project of projects) {
+    projectPromises.push(
+      (async () => {
+        let ab = await (
+          await fetch(`https://projects.scratch.mit.edu/${project.id}`)
+        ).arrayBuffer();
+        const name = `${folder}/${project.title.replace(
+          /[/\\?%*:|"<>]/g,
+          "-"
+        )}`;
+        if (fs.existsSync(name)) {
+          CURRENT[project.id] =
+            `File already exists (${project.title.brightBlue})`.brightYellow;
+        }
+        CURRENT[project.id] = "Loading project".brightBlue;
+        let loaded = await Packager.loadProject(ab, (type, a, b) => {
+          CURRENT[project.id] = `${`[${type}]`.yellow} ${
+            typeof a === "string" ? a : `${a * 100}%`.brightGreen
+          }`;
+        });
+        CURRENT[project.id] = "Packaging".brightBlue;
+        const packager = new Packager.Packager(loaded);
+        let f = Object.fromEntries(
+          Object.entries(features).map(([k, v]) => [
+            k,
+            projectOptions.features.includes(v),
+          ])
+        );
+        f = {
+          loadingScreen: {
+            progressBar: f.loadingBar,
+          },
+          compiler: {
+            enabled: f.compilerEnable,
+          },
+        };
+        Object.assign(packager.options, f);
+        packager.project = loaded;
+        const { type, data } = await packager.package();
+        CURRENT[project.id] = "Writing file";
+        fs.writeFileSync(`${name}.${type.split("/")[1]}`, data);
+        CURRENT[project.id] = "Finished".brightGreen;
+      })()
+    );
+  }
+
+  Promise.all(projectPromises).then(
+    (p) => ((projects = p), (CURRENT.status = "Finished"), process.exit(0))
+  );
+
+  function log() {
+    let spinner = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
+
+    spinner = `${spinner[Math.floor(Date.now() / 50) % spinner.length]}`;
+
+    let logThis = `${spinner} ${CURRENT.status}`.yellow;
+
+    if (Object.entries(CURRENT.projects).length) {
+      logThis += "\n\n";
+      logThis += Object.entries(CURRENT.projects)
+        .map(([k, v]) => `${`[${k}]`.yellow} ${v}`)
+        .join("\n");
+    }
+
+    logUpdate(logThis);
+  }
+})();
+
+/* (async () => {
 	const buttons = {
 		startStop: "Start and stop buttons",
 		fullscreen: "Fullscreen button",
@@ -210,4 +425,5 @@ import logUpdate from 'log-update';
 			continue;
 		}
 	}
-})();
+})
+ */
