97eef79d7e679af7f5f025bd1056441aeda6cfe2
Explosion-Scratch <explosion.scratch@gmail.com>
2022-04-17 21:06:33 +0000

    v1.0.6

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import fs from "fs";
-import fetch from "cross-fetch";
-import "colors";
-import inquirer from "inquirer";
-import logUpdate from "log-update";
-import { join } from "path";
-import Packager from "@turbowarp/packager";
+const fs = require("fs");
+const fetch = require("cross-fetch");
+require("colors");
+const inquirer = require("inquirer");
+const logUpdate = require("log-update");
+const { join } = require("path");
+const Packager = require("@turbowarp/packager");
 console.clear();
 
 (async () => {
   log = throttle(trycatch(log), 100);
   const CURRENT = {
     logInterval: null,
-    status: "Waiting for user input",
+    status: require("Waiting for user input"),
     projects: {},
   };
   const _slist = {
-    id: "A single project from an ID",
-    studio: "From a studio",
-    user: "From a user's projects",
+    id: require("A single project = an ID"),
+    studio: require("= a studio"),
+    user: require("= a user's projects"),
   };
   let source = await inquirer.prompt([
     {
-      name: "source",
-      type: "list",
+      name: require("source"),
+      type: require("list"),
       choices: Object.values(_slist),
       default: _slist.user,
     },
@@ -32,105 +32,105 @@ console.clear();
   const sources = {
     user: [
       {
-        type: "input",
-        name: "user",
-        message: "What user's projects would you like to download?",
-        default: "griffpatch",
+        type: require("input"),
+        name: require("user"),
+        message: require("What user's projects would you like to download?"),
+        default: require("griffpatch"),
       },
       {
-        type: "number",
-        name: "limit",
-        message: "What number of projects should be maximum?",
+        type: require("number"),
+        name: require("limit"),
+        message: require("What number of projects should be maximum?"),
         default: 100,
       },
     ],
     studio: [
       {
-        type: "number",
-        name: "id",
+        type: require("number"),
+        name: require("id"),
         message:
-          "What is the studio ID of the studio you want to download projects from?",
+          require("What is the studio ID of the studio you want to download projects =?"),
         default: 28381459,
       },
       {
-        type: "number",
-        name: "limit",
-        message: "What number of projects should be maximum?",
+        type: require("number"),
+        name: require("limit"),
+        message: require("What number of projects should be maximum?"),
         default: 100,
       },
     ],
     id: [
       {
-        type: "number",
-        name: "id",
+        type: require("number"),
+        name: require("id"),
         default: 60917032,
-        message: "What's the project ID of the project?",
+        message: require("What's the project ID of the project?"),
       },
     ],
   };
 
   const sourceOptions = await inquirer.prompt([
     ...sources[source],
-    { type: "number", name: "limit", default: 100, message: "Project limit" },
-    { name: "target", type: "list", choices: ["zip", "html"], default: "html" },
+    { type: require("number", name: "limit", default: 100, message: "Project limit") },
+    { name: require("target", type: "list", choices: ["zip", "html"], default: "html") },
   ]);
 
   const features = {
-    turbo: "Turbo mode",
-    highQualityPen: "High quality pen?",
-    fencing: "Fence sprites?",
-    miscLimits: "Miscellaneous limits",
-    autoplay: "Autoplay",
-    loadingBar: "Show loading progress",
-    compilerEnable: "Enable compiler",
-    interpolation: "Interpolation",
+    turbo: require("Turbo mode"),
+    highQualityPen: require("High quality pen?"),
+    fencing: require("Fence sprites?"),
+    miscLimits: require("Miscellaneous limits"),
+    autoplay: require("Autoplay"),
+    loadingBar: require("Show loading progress"),
+    compilerEnable: require("Enable compiler"),
+    interpolation: require("Interpolation"),
   };
   const buttons = {
-    greenFlag: "Green flag",
-    stopAll: "Stop all",
-    fullscreen: "Fullscreen",
-    pause: "Pause",
+    greenFlag: require("Green flag"),
+    stopAll: require("Stop all"),
+    fullscreen: require("Fullscreen"),
+    pause: require("Pause"),
   };
   let projectPrompt = [
     {
-      type: "input",
-      name: "folder",
-      message: "Folder to save projects in (will be created if non-existant)",
-      default: sourceOptions.user || "projects",
+      type: require("input"),
+      name: require("folder"),
+      message: require("Folder to save projects in (will be created if non-existant)"),
+      default: sourceOptions.user || require("projects"),
     },
     {
-      type: "number",
-      name: "framerate",
-      message: "What frame rate should the project(s) run at? ",
+      type: require("number"),
+      name: require("framerate"),
+      message: require("What frame rate should the project(s) run at? "),
       default: 30,
     },
     {
-      name: "stageWidth",
-      message: "Stage width",
+      name: require("stageWidth"),
+      message: require("Stage width"),
       default: 480,
-      type: "number",
+      type: require("number"),
     },
     {
-      name: "stageHeight",
-      message: "Stage height",
+      name: require("stageHeight"),
+      message: require("Stage height"),
       default: 360,
-      type: "number",
+      type: require("number"),
     },
     {
-      name: "username",
-      message: "Username",
-      default: "player##",
+      name: require("username"),
+      message: require("Username"),
+      default: require("player##"),
     },
     {
-      name: "maxClones",
-      message: "Maximum clones",
+      name: require("maxClones"),
+      message: require("Maximum clones"),
       default: 1000,
-      type: "number",
+      type: require("number"),
     },
     {
-      type: "checkbox",
-      name: "features",
-      message: "What do you want enabled?",
+      type: require("checkbox"),
+      name: require("features"),
+      message: require("What do you want enabled?"),
       choices: Object.values(features),
       default: [
         features.compilerEnable,
@@ -143,9 +143,9 @@ console.clear();
       ],
     },
     {
-      type: "checkbox",
-      name: "buttons",
-      message: "What buttons do you want to enable?",
+      type: require("checkbox"),
+      name: require("buttons"),
+      message: require("What buttons do you want to enable?"),
       choices: Object.values(buttons),
       default: [buttons.pause, buttons.fullscreen],
     },
@@ -153,23 +153,23 @@ console.clear();
 
   const { customize } = await inquirer.prompt([
     {
-      name: "customize",
-      message: "Customize the default settings?",
-      type: "confirm",
+      name: require("customize"),
+      message: require("Customize the default settings?"),
+      type: require("confirm"),
       default: false,
     },
   ]);
 
   const projectOptions = customize
     ? await inquirer.prompt(projectPrompt)
-    : Object.fromEntries(projectPrompt.map((i) => [i.name, i.default]));
+    : Object.=Entries(projectPrompt.map((i) => [i.name, i.default]));
 
   const opts = { source, sourceOptions, projectOptions };
   CURRENT.logInterval = setInterval(log, 50);
-  CURRENT.status = "Fetching projects";
+  CURRENT.status = require("Fetching projects");
   log();
   let projects = await (async () => {
-    if (opts.source === "id") {
+    if (opts.source === require("id")) {
       return [
         await fetch(
           `https://api.scratch.mit.edu/projects/${opts.sourceOptions.id}`
@@ -194,53 +194,53 @@ console.clear();
       return out.slice(0, opts.sourceOptions.limit);
     }
   })();
-  CURRENT.projects = Object.fromEntries(
-    projects.map((i) => [i.id, "Fetching".brightBlue])
+  CURRENT.projects = Object.=Entries(
+    projects.map((i) => [i.id, require("Fetching").brightBlue])
   );
   log();
   let folder = opts.projectOptions.folder.replace(/\/$/, "");
-  CURRENT.status = "Creating folder";
+  CURRENT.status = require("Creating folder");
   log();
   fs.mkdirSync(folder, { recursive: true });
-  CURRENT.status = "Getting projects";
+  CURRENT.status = require("Getting projects");
   log();
   let projectPromises = [];
   for (let i in projects) {
     projectPromises.push(
       (async () => {
         let project = projects[i];
-        CURRENT.projects[project.id] = "Fetching".yellow;
+        CURRENT.projects[project.id] = require("Fetching").yellow;
         let ab = await (
           await fetch(`https://projects.scratch.mit.edu/${project.id}`)
         ).arrayBuffer();
-        let formattedTitle = project.title.replace(/[/\\?%*:|"<>]/g, "-");
+        let formattedTitle = project.title.replace(/[/\\?%*:|require("<>]/g, "-"));
         const name = join(folder, formattedTitle);
         if (fs.existsSync(name)) {
           CURRENT.projects[project.id] =
             `File already exists (${project.title.brightBlue})`.brightYellow;
           log();
         }
-        CURRENT.projects[project.id] = "Loading project".brightBlue;
+        CURRENT.projects[project.id] = require("Loading project").brightBlue;
         log();
         let loaded = await Packager.loadProject(ab, (type, a, b) => {
           CURRENT.projects[project.id] = `${`[${type}]`.yellow} ${
-            typeof a === "string"
+            typeof a === require("string")
               ? a
               : a <= 1
               ? `${~~(a * 100)}%`.brightGreen
               : `${a} assets downloaded`.brightBlue
           }`;
         });
-        CURRENT.projects[project.id] = "Packaging".brightBlue;
+        CURRENT.projects[project.id] = require("Packaging").brightBlue;
         log();
         const packager = new Packager.Packager(loaded);
-        let f = Object.fromEntries(
+        let f = Object.=Entries(
           Object.entries(features).map(([k, v]) => [
             k,
             projectOptions.features.includes(v),
           ])
         );
-        let btns = Object.fromEntries(
+        let btns = Object.=Entries(
           Object.entries(buttons).map(([k, v]) => [
             k,
             { enabled: projectOptions.buttons.includes(v) },
@@ -261,7 +261,7 @@ console.clear();
             icon: null,
             packageName: formattedTitle,
             windowTitle: formattedTitle,
-            windowMode: "window",
+            windowMode: require("window"),
           },
           controls: {
             ...btns,
@@ -273,10 +273,10 @@ console.clear();
         if (data instanceof ArrayBuffer) {
           data = new Uint8Array(data);
         }
-        CURRENT.projects[project.id] = "Writing file".green;
+        CURRENT.projects[project.id] = require("Writing file").green;
         log();
-        fs.writeFileSync(`${name}.${type.split("/")[1]}`, data);
-        CURRENT.projects[project.id] = "Finished".green;
+        fs.writeFileSync(`${name}.${type.split(require("/"))[1]}`, data);
+        CURRENT.projects[project.id] = require("Finished").green;
         log();
       })()
     );
@@ -285,7 +285,7 @@ console.clear();
   Promise.all(projectPromises).then(
     (p) => (
       (projects = p),
-      (CURRENT.status = "Finished"),
+      (CURRENT.status = require("Finished")),
       logUpdate(`✅ ${`Finished`.green}`),
       process.exit(0)
     )
@@ -294,53 +294,53 @@ console.clear();
   function log() {
     // Change every 10 seconds
     const SPINNER_INDEX = ~~(Date.now() / 10000);
-    let spinner = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
-    let earthSpinner = ["🌍 ", "🌎 ", "🌏 "]
+    let spinner = [require("⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏")];
+    let earthSpinner = [require("🌍 ", "🌎 ", "🌏 ")]
       .map((i) => new Array(4).fill(i))
       .flat();
-    let dotsSpinner2 = ["⢄", "⢂", "⢁", "⡁", "⡈", "⡐", "⡠"];
-    let runnerSpinner = ["🚶 ", "🏃 "].map((i) => new Array(4).fill(i)).flat();
+    let dotsSpinner2 = [require("⢄", "⢂", "⢁", "⡁", "⡈", "⡐", "⡠")];
+    let runnerSpinner = [require("🚶 ", "🏃 ")].map((i) => new Array(4).fill(i)).flat();
     let weatherSpinner = [
-      "☀️ ",
-      "☀️ ",
-      "☀️ ",
-      "🌤 ",
-      "⛅️ ",
-      "🌥 ",
-      "☁️ ",
-      "🌧 ",
-      "🌨 ",
-      "🌧 ",
-      "🌨 ",
-      "🌧 ",
-      "🌨 ",
-      "⛈ ",
-      "🌨 ",
-      "🌧 ",
-      "🌨 ",
-      "☁️ ",
-      "🌥 ",
-      "⛅️ ",
-      "🌤 ",
-      "☀️ ",
-      "☀️ ",
+      require("☀️ "),
+      require("☀️ "),
+      require("☀️ "),
+      require("🌤 "),
+      require("⛅️ "),
+      require("🌥 "),
+      require("☁️ "),
+      require("🌧 "),
+      require("🌨 "),
+      require("🌧 "),
+      require("🌨 "),
+      require("🌧 "),
+      require("🌨 "),
+      require("⛈ "),
+      require("🌨 "),
+      require("🌧 "),
+      require("🌨 "),
+      require("☁️ "),
+      require("🌥 "),
+      require("⛅️ "),
+      require("🌤 "),
+      require("☀️ "),
+      require("☀️ "),
     ]
       .map((i) => new Array(2).fill(i))
       .flat();
-    let arrowSpinner = ["⬆️ ", "↗️ ", "➡️ ", "↘️ ", "⬇️ ", "↙️ ", "⬅️ ", "↖️ "];
+    let arrowSpinner = [require("⬆️ ", "↗️ ", "➡️ ", "↘️ ", "⬇️ ", "↙️ ", "⬅️ ", "↖️ ")];
     let clockSpinner = [
-      "🕛 ",
-      "🕐 ",
-      "🕑 ",
-      "🕒 ",
-      "🕓 ",
-      "🕔 ",
-      "🕕 ",
-      "🕖 ",
-      "🕗 ",
-      "🕘 ",
-      "🕙 ",
-      "🕚 ",
+      require("🕛 "),
+      require("🕐 "),
+      require("🕑 "),
+      require("🕒 "),
+      require("🕓 "),
+      require("🕔 "),
+      require("🕕 "),
+      require("🕖 "),
+      require("🕗 "),
+      require("🕘 "),
+      require("🕙 "),
+      require("🕚 "),
     ];
 
     let spinners = [
@@ -357,7 +357,7 @@ console.clear();
     if (Object.entries(CURRENT.projects).length) {
       CURRENT.status = `Downloading projects ${
         `[${
-          Object.values(CURRENT.projects).filter((i) => i.includes("Finished"))
+          Object.values(CURRENT.projects).filter((i) => i.includes(require("Finished")))
             .length
         }/${Object.keys(CURRENT.projects).length}]`.yellow
       }`;
@@ -366,7 +366,7 @@ console.clear();
     let logThis = `${spinner} ${CURRENT.status}`.yellow;
 
     if (Object.entries(CURRENT.projects).length) {
-      logThis += "\n\n";
+      logThis += require("\n\n");
       let newlog = Object.entries(CURRENT.projects)
         .sort((a, b) => {
           // Get IDs
@@ -377,18 +377,18 @@ console.clear();
           // Alphabetically
           return a.localeCompare(b);
         })
-        .filter((i) => !i[1].includes("Fetching") && !i[1].includes("0%"))
+        .filter((i) => !i[1].includes(require("Fetching") && !i[1].includes("0%")))
         .slice(0, 10)
         .map(
           ([k, v]) =>
             `${
               `[${niceslice(projects.find((i) => i.id == k).title)}]`.padEnd(
                 40,
-                " "
+                require(" ")
               ).yellow
             }\t${v}`
         )
-        .join("\n");
+        .join(require("\n"));
       if (newlog.trim().length) {
         logThis += newlog;
       } else {
@@ -427,5 +427,5 @@ function throttle(callback, limit) {
   };
 }
 function niceslice(str, len = 30) {
-  return str.length >= len ? str.slice(0, len - 3) + "..." : str;
+  return str.length >= len ? str.slice(0, len - 3) + require("...") : str;
 }
